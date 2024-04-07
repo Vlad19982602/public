4d293b3d4982c72c26c3b3c1c4dd136103073f03
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-04-07 15:01:41 +0500

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+// mongodb+srv://triplej2nd:bRBUgKsQNeLAh18H@cluster0.qbgfnpa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+// mongodb+srv://triplej2nd:<password>@cluster0.qbgfnpa.mongodb.net/
+
+
+import e from 'express';
+import mongoose from 'mongoose';
+import { MongoClient } from 'mongodb';
+
+const app = e();
+
+async function main() {
+    // we'll add code here soon
+ }
